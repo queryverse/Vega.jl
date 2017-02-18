051b56d5d395af14f36e4f5e88afcace3bfb71eb
fredo <frederic.testard@gmail.com>
2017-02-18 19:56:23 +0100

    added plotting within Atom plotpane

--- src/atom_integration.jl
+++ src/atom_integration.jl
@@ -9,78 +9,32 @@
 
 @require Atom begin  # define only if/when Atom is loaded
 
-  function writemime(io::IO, m::MIME"text/html", v::VegaLiteVis)
-    divid = "vl" * randstring(10) # generated id for this plot
-
-    fh = """
-    <div>
-      <div id="$divid"></div>
-
-      <style media="screen">
-        .vega-actions a {
-          margin-right: 10px;
-          font-family: sans-serif;
-          font-size: x-small;
-          font-style: italic;
-        }
-      </style>
-
-      <meta charset="utf-8">
-
-      <script type="text/javascript">
-        require.config({
-          paths: {
-            d3:        "https://d3js.org/d3.v3.min",
-            vega:      "https://vega.github.io/vega/vega",
-            vegalite:  "https://vega.github.io/vega-lite/vega-lite",
-            vegaembed: "https://vega.github.io/vega-editor/vendor/vega-embed"
-          }
-        });
-
-        require(["d3"], function(d3){
-          window.d3 = d3;
-
-          require(["vega"], function(vg) {
-            window.vg = vg
-
-            require(["vegalite"], function(vgl) {
-
-              require(["vegaembed"], function(vge) {
-
-                var embedSpec = {
-                  mode: "vega-lite",
-                  renderer: "$(SVG ? "svg" : "canvas")",
-                  actions: $SAVE_BUTTONS,
-                  spec: $(JSON.json(v.vis))
-                }
-
-            //    vg.embed("#$divid", embedSpec, function(error, result) {
-            //    });
-
-                var vgSpec = vgl.compile(embedSpec.spec).spec;
-                vg.parse.spec(vgSpec, function(chart) { chart({el:\"#$divid\"}).update(); });
-
-              });
-            });
-          });
-        });
-
-      </script>
-
-    </div>
-    """
-    # FIXME : understand why vega-embed can't be loaded
-    #   SVG and SAVE_BUTTONS flags are ignored
-    # vg.embed("#$divid", embedSpec, function(error, result) {
-
-    write(io, fh)
-  end
   import Atom, Media
 
   Media.media(VegaLiteVis, Media.Plot)
 
   function Media.render(e::Atom.Editor, plt::VegaLiteVis)
+
     Media.render(e, nothing)
+  end
+
+  # tests if wkhtmltoimage is in the path, if yes define rendering in plot pane
+  try
+    run(`wkhtmltoimage -V`)
+
+    function Media.render(pane::Atom.PlotPane, plt::VegaLiteVis)
+      # create a temporary file
+      tmppath = string(tempname(), ".vegalite.html")
+      open(tmppath, "w") do io
+        writehtml(io, plt)
+      end
+
+      sz = (400,400)
+      png_fn = string(tempname(), ".png")
+      run(`wkhtmltoimage -f png -q --width $(sz[1]) --height $(sz[2]) $tmppath $png_fn`)
+
+      Media.render(pane, Atom.div(Atom.img(src=png_fn)))
+    end
   end
 
 end
