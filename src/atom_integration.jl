fea2f01839013cb52ce0def06e48fcfe8dead4a7
fredo <frederic.testard@gmail.com>
2017-03-12 11:32:59 +0100

    forgot to remowe comments

--- src/atom_integration.jl
+++ src/atom_integration.jl
@@ -17,27 +17,27 @@
   end
 
   # tests if wkhtmltoimage is in the path, if yes define rendering in plot pane
-  # try
-  #   run(`wkhtmltoimage -V`)
-  #
-  #   Media.media(VegaLiteVis, Media.Plot)
-  #
-  #   Media.render(e::Atom.Editor, plt::VegaLiteVis) =
-  #     Media.render(e, nothing)
-  #
-  #   function Media.render(pane::Atom.PlotPane, plt::VegaLiteVis)
-  #     # create a temporary file
-  #     tmppath = string(tempname(), ".vegalite.html")
-  #     open(tmppath, "w") do io
-  #       writehtml(io, plt)
-  #     end
-  #
-  #     sz = (400,400)
-  #     png_fn = string(tempname(), ".png")
-  #     run(`wkhtmltoimage -f png -q --width $(sz[1]) --height $(sz[2]) $tmppath $png_fn`)
-  #
-  #     Media.render(pane, Atom.div(Atom.img(src=png_fn)))
-  #   end
-  # end
+  try
+    run(`wkhtmltoimage -V`)
+
+    Media.media(VegaLiteVis, Media.Plot)
+
+    Media.render(e::Atom.Editor, plt::VegaLiteVis) =
+      Media.render(e, nothing)
+  
+    function Media.render(pane::Atom.PlotPane, plt::VegaLiteVis)
+      # create a temporary file
+      tmppath = string(tempname(), ".vegalite.html")
+      open(tmppath, "w") do io
+        writehtml(io, plt)
+      end
+
+      sz = (400,400)
+      png_fn = string(tempname(), ".png")
+      run(`wkhtmltoimage -f png -q --width $(sz[1]) --height $(sz[2]) $tmppath $png_fn`)
+
+      Media.render(pane, Atom.div(Atom.img(src=png_fn)))
+    end
+  end
 
 end
