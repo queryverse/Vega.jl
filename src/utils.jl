865e1f9fc23461ac3951a1ac15ff49ec229277a9
David Anthoff <anthoff@berkeley.edu>
2017-06-05 10:47:28 -0700

    Add support for the pipe operator

--- src/utils.jl
+++ src/utils.jl
@@ -9,6 +9,10 @@ end
 +(a::VegaLiteVis, b::VegaLiteVis) = VegaLiteVis(softmerge(a.vis, b.vis))
 *(a::VegaLiteVis, b::VegaLiteVis) = VegaLiteVis(merge(a.vis, b.vis))
 
+function (a::VegaLiteVis)(b::VegaLiteVis)
+    return a + b
+end
+
 function softmerge(a::Dict, b::Dict)
     ck = intersect(keys(a), keys(b))
     nd = merge(a, b)
