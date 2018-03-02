d3680d2c46d70ab0acddcaab8b98fb4581116024
fredo <frederic.testard@gmail.com>
2018-03-02 15:44:14 +0100

    update to vegalite 2.1.3, made schema downloaded during build phase

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,9 @@
-# download javascript files
+# download spec schema, javascript files, and installs nodes js packages
 
 using BinDeps
 @BinDeps.setup
 
-# uschema = "https://vega.github.io/schema/vega-lite/v2.json"
-
+uschema   = "https://vega.github.io/schema/vega-lite/v2.1.3.json"
 uvega     = "https://cdnjs.cloudflare.com/ajax/libs/vega/3.1.0/vega.min.js"
 uvegalite = "https://cdnjs.cloudflare.com/ajax/libs/vega-lite/2.1.3/vega-lite.min.js"
 uembed    = "https://cdnjs.cloudflare.com/ajax/libs/vega-embed/3.0.0/vega-embed.min.js"
@@ -13,7 +12,7 @@ destdir      = joinpath(@__DIR__, "lib")
 
 run(@build_steps begin
   CreateDirectory(destdir, true)
-  # FileDownloader(uschema,   joinpath(destdir, basename(uschema)))
+  FileDownloader(uschema,   joinpath(destdir, "vega-lite-schema.json"))
   FileDownloader(uvega,     joinpath(destdir, basename(uvega)))
   FileDownloader(uvegalite, joinpath(destdir, basename(uvegalite)))
   FileDownloader(uembed,    joinpath(destdir, basename(uembed)))
