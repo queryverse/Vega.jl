1ab193ff0979c25e7da74af88ed42d1705a65afe
David Anthoff <anthoff@berkeley.edu>
2018-06-01 22:21:48 -0700

    Update documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,27 @@
 using Documenter, VegaLite
 
+info("Building docs...")
 
 makedocs(
   modules=[VegaLite],
   format=:html,
   sitename = "VegaLite.jl",
   pages = [
-    "Introduction" => "index.md",
-    "Quick Tour" => "quick.md",
-    "Vega-lite specifications" => "vlspec.md",
-    "Global settings" => "global.md",
-    "Outputs" => "output.md",
-    "API reference" => "functions.md"]
+    "Home" => "index.md",
+    "Getting Started" => Any[
+        "Quick Tour" => "gettingstarted/quick.md"
+    ],
+    "Examples" => Any[
+        "Simple Charts" => "examples/examples_simplecharts.md"
+    ],
+    "User Guide" => Any[
+        "Vega-lite specifications" => "userguide/vlspec.md"
+    ],
+    "Reference Manual" => [
+        "Global settings" => "referencemanual/global.md",
+        "Outputs" => "referencemanual/output.md",
+        "API reference" => "referencemanual/functions.md"]
+  ]
 )
 
 deploydocs(
