f1f8ba3731b3e8d8bee641cb2e3f12b73250dab1
Robert Schwarz <mail@rschwarz.net>
2018-11-21 22:24:58 +0100

    refactor: add set_spec_data!

--- src/vlspec.jl
+++ src/vlspec.jl
@@ -9,6 +9,11 @@ struct VLSpec{T}
 end
 vltype(::VLSpec{T}) where T = T
 
+function set_spec_data!(specdict, datait)
+    recs = [Dict{String,Any}(string(c[1])=>isa(c[2], DataValues.DataValue) ? (isna(c[2]) ? nothing : get(c[2])) : c[2] for c in zip(keys(r), values(r))) for r in datait]
+    specdict["data"] = Dict{String,Any}("values" => recs)
+end
+
 function detect_encoding_type!(specdict, datait)
     col_names = fieldnames(eltype(datait))
     col_types = [fieldtype(eltype(datait),i) for i in col_names]
@@ -38,13 +43,10 @@ end
 function (p::VLSpec{:plot})(data)
     TableTraits.isiterabletable(data) || throw(ArgumentError("'data' is not a table."))
 
-    it = IteratorInterfaceExtensions.getiterator(data)
-
-    recs = [Dict{String,Any}(string(c[1])=>isa(c[2], DataValues.DataValue) ? (isna(c[2]) ? nothing : get(c[2])) : c[2] for c in zip(keys(r), values(r))) for r in it]
-
     new_dict = copy(p.params)
-    new_dict["data"] = Dict{String,Any}("values" => recs)
 
+    it = IteratorInterfaceExtensions.getiterator(data)
+    set_spec_data!(new_dict, it)
     detect_encoding_type!(new_dict, it)
 
     return VLSpec{:plot}(new_dict)        
