6848ca9aa5c1d492b4a49f2ceb784dd40cfa38da
David Anthoff <anthoff@berkeley.edu>
2019-03-25 16:09:15 -0400

    Update dependencies

--- deps/build.jl
+++ deps/build.jl
@@ -3,10 +3,10 @@
 using BinDeps
 @BinDeps.setup
 
-uschema   = "https://vega.github.io/schema/vega-lite/v2.6.0.json"
-uvega     = "https://cdnjs.cloudflare.com/ajax/libs/vega/4.4.0/vega.min.js"
-uvegalite = "https://cdnjs.cloudflare.com/ajax/libs/vega-lite/2.6.0/vega-lite.min.js"
-uembed    = "https://cdnjs.cloudflare.com/ajax/libs/vega-embed/3.29.1/vega-embed.min.js"
+uschema   = "https://vega.github.io/schema/vega-lite/v3.0.0.json"
+uvega     = "https://cdnjs.cloudflare.com/ajax/libs/vega/5.3.2/vega.min.js"
+uvegalite = "https://cdnjs.cloudflare.com/ajax/libs/vega-lite/3.0.0/vega-lite.min.js"
+uembed    = "https://cdnjs.cloudflare.com/ajax/libs/vega-embed/4.0.0/vega-embed.min.js"
 
 destdir      = joinpath(@__DIR__, "lib")
 
