38a17dc46428c41cc125b637220064385d72991a
David Anthoff <anthoff@berkeley.edu>
2019-06-11 09:17:03 -0700

    Finish migration to node-canvas

--- src/rendering/show.jl
+++ src/rendering/show.jl
@@ -3,22 +3,6 @@ function Base.show(io::IO, m::MIME"text/plain", v::AbstractVegaSpec)
     print(io, summary(v))
 end
 
-function convert_to_svg(v::AbstractVegaSpec, script_path::String)
-    data = JSON.json(v.params)
-    p = open(`$(nodejs_cmd()) $script_path`, "r+")
-    writer = @async begin
-        write(p, data)
-        close(p.in)
-    end
-    reader = @async read(p, String)
-    wait(p)
-    res = fetch(reader)
-    if p.exitcode!=0
-        throw(ArgumentError("Invalid spec"))
-    end
-    return res
-end
-
 function convert_vl_to_vg(v::VLSpec{:plot})
     vl2vg_script_path = joinpath(@__DIR__, "vl2vg.js")
     data = JSON.json(v.params)
@@ -54,15 +38,21 @@ function convert_vl_to_x(v::VLSpec{:plot}, second_script)
     return res
 end
 
-
-function convert_to_svg(v::VLSpec{:plot})
-    script_path = joinpath(@__DIR__, "compilesvg.js")
-    return convert_to_svg(v, script_path)
-end
-
-function convert_to_svg(v::VGSpec)
-    script_path = joinpath(@__DIR__, "compilevg2svg.js")
-    return convert_to_svg(v, script_path)
+function convert_vg_to_x(v::VGSpec, script)
+    full_script_path = joinpath(@__DIR__, "..", "..", "deps", "node_modules", "vega-cli", "bin", script)
+    data = JSON.json(v.params)
+    p = open(`$(nodejs_cmd()) $full_script_path`, "r+")
+    writer = @async begin
+        write(p, data)
+        close(p.in)
+    end
+    reader = @async read(p, String)
+    wait(p)
+    res = fetch(reader)
+    if p.exitcode!=0
+        throw(ArgumentError("Invalid spec"))
+    end
+    return res
 end
 
 Base.Multimedia.istextmime(::MIME{Symbol("application/vnd.vegalite.v3+json")}) = true
@@ -82,42 +72,37 @@ function Base.show(io::IO, m::MIME"application/vnd.vega.v5+json", v::VLSpec{:plo
 end
 
 function Base.show(io::IO, m::MIME"image/svg+xml", v::VLSpec{:plot})
-    @info "YES"
     print(io, convert_vl_to_x(v, "vg2svg"))
 end
 
-function Base.show(io::IO, m::MIME"application/pdf", v::AbstractVegaSpec)
-    svgstring = convert_to_svg(v)
-
-    r = Rsvg.handle_new_from_data(svgstring)
-    d = Rsvg.handle_get_dimensions(r)
-
-    cs = Cairo.CairoPDFSurface(io, d.width,d.height)
-    c = Cairo.CairoContext(cs)
-    Rsvg.handle_render_cairo(c,r)
-    Cairo.finish(cs)
+function Base.show(io::IO, m::MIME"image/svg+xml", v::VGSpec)
+    print(io, convert_vg_to_x(v, "vg2svg"))
 end
 
-function Base.show(io::IO, m::MIME"application/eps", v::AbstractVegaSpec)
-    svgstring = convert_to_svg(v)
-
-    r = Rsvg.handle_new_from_data(svgstring)
-    d = Rsvg.handle_get_dimensions(r)
-
-    cs = Cairo.CairoEPSSurface(io, d.width,d.height)
-    c = Cairo.CairoContext(cs)
-    Rsvg.handle_render_cairo(c,r)
-    Cairo.finish(cs)
+function Base.show(io::IO, m::MIME"application/pdf", v::VLSpec{:plot})
+    print(io, convert_vl_to_x(v, "vg2pdf"))
 end
 
-function Base.show(io::IO, m::MIME"image/png", v::AbstractVegaSpec)
-    svgstring = convert_to_svg(v)
+function Base.show(io::IO, m::MIME"application/pdf", v::VGSpec)
+    print(io, convert_vg_to_x(v, "vg2pdf"))
+end
 
-    r = Rsvg.handle_new_from_data(svgstring)
-    d = Rsvg.handle_get_dimensions(r)
+# function Base.show(io::IO, m::MIME"application/eps", v::AbstractVegaSpec)
+#     svgstring = convert_to_svg(v)
 
-    cs = Cairo.CairoImageSurface(d.width,d.height,Cairo.FORMAT_ARGB32)
-    c = Cairo.CairoContext(cs)
-    Rsvg.handle_render_cairo(c,r)
-    Cairo.write_to_png(cs,io)
+#     r = Rsvg.handle_new_from_data(svgstring)
+#     d = Rsvg.handle_get_dimensions(r)
+
+#     cs = Cairo.CairoEPSSurface(io, d.width,d.height)
+#     c = Cairo.CairoContext(cs)
+#     Rsvg.handle_render_cairo(c,r)
+#     Cairo.finish(cs)
+# end
+
+function Base.show(io::IO, m::MIME"image/png", v::VLSpec{:plot})
+    print(io, convert_vl_to_x(v, "vg2png"))
+end
+
+function Base.show(io::IO, m::MIME"image/png", v::VGSpec)
+    print(io, convert_vg_to_x(v, "vg2png"))
 end
