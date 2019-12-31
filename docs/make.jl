28b47ce55f078df9eeb2e85b2a6031e39a97c295
David Anthoff <anthoff@berkeley.edu>
2019-12-30 17:05:06 -0800

    Add a level in the documentation

--- docs/make.jl
+++ docs/make.jl
@@ -32,23 +32,25 @@ makedocs(
         "Using Vega" => "userguide/vega.md"
     ],
     "Examples" => Any[
-        "Simple Charts" => "examples/examples_simplecharts.md",
-        "Single-View Plots" => Any[
-            "Bar Charts & Histograms" => "examples/examples_barchartshistograms.md",
-            "Scatter & Strip Plots" => "examples/examples_scatter_strip_plots.md",
-            "Line Charts" => "examples/examples_line_charts.md",
-            "Area Charts & Streamgraphs" => "examples/examples_area_Charts_streamgraphs.md",
-            "Table-based Plots" => "examples/examples_table_based_plots.md"
+        "Vega-Lite" => [
+            "Simple Charts" => "examples/examples_simplecharts.md",
+            "Single-View Plots" => Any[
+                "Bar Charts & Histograms" => "examples/examples_barchartshistograms.md",
+                "Scatter & Strip Plots" => "examples/examples_scatter_strip_plots.md",
+                "Line Charts" => "examples/examples_line_charts.md",
+                "Area Charts & Streamgraphs" => "examples/examples_area_Charts_streamgraphs.md",
+                "Table-based Plots" => "examples/examples_table_based_plots.md"
+            ],
+            "Composite Mark" => Any[
+                "Error Bars & Error Bands" => "examples/examples_error_bars_bands.md",
+                "Box Plots" => "examples/examples_box_plots.md"
+            ],
+            "Multi-View Displays" => Any[
+                "Faceting (Trellis Plot / Small Multiples)" => "examples/examples_faceting.md",
+                "Repeat & Concatenation" => "examples/examples_repeat_concatenation.md"
+            ],
+            "Maps (Geographic Displays)" => "examples/examples_maps.md"
         ],
-        "Composite Mark" => Any[
-            "Error Bars & Error Bands" => "examples/examples_error_bars_bands.md",
-            "Box Plots" => "examples/examples_box_plots.md"
-        ],
-        "Multi-View Displays" => Any[
-            "Faceting (Trellis Plot / Small Multiples)" => "examples/examples_faceting.md",
-            "Repeat & Concatenation" => "examples/examples_repeat_concatenation.md"
-        ],
-        "Maps (Geographic Displays)" => "examples/examples_maps.md",
         "Vega" => [
             "Bar Charts" => "examples/examples_vega_bar_charts.md",
             "Line & Area Charts" => "examples/examples_vega_line_area_charts.md",
