96dff49be19f37fc075a34d4b3de41a0c400d6a5
David Anthoff <anthoff@berkeley.edu>
2020-04-06 20:37:53 -0700

    Make docs use latest vega assets

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, Vega, UUIDs
 function Base.show(io::IO, m::MIME"text/html", v::Vega.VGSpec)
     divid = string("vg", replace(string(uuid4()), "-"=>""))
     print(io, "<div id='$divid' style=\"width:100%;height:100%;\"></div>")
-    print(io, "<script type='text/javascript'>requirejs.config({paths:{'vg-embed': 'https://cdn.jsdelivr.net/npm/vega-embed@6.2.1?noext','vega-lib': 'https://cdn.jsdelivr.net/npm/vega-lib?noext','vega-lite': 'https://cdn.jsdelivr.net/npm/vega-lite@4.0.2?noext','vega': 'https://cdn.jsdelivr.net/npm/vega@5.9.0?noext'}}); require(['vg-embed'],function(vegaEmbed){vegaEmbed('#$divid',")    
+    print(io, "<script type='text/javascript'>requirejs.config({paths:{'vg-embed': 'https://cdn.jsdelivr.net/npm/vega-embed@6?noext','vega-lib': 'https://cdn.jsdelivr.net/npm/vega-lib?noext','vega-lite': 'https://cdn.jsdelivr.net/npm/vega-lite@4?noext','vega': 'https://cdn.jsdelivr.net/npm/vega@5?noext'}}); require(['vg-embed'],function(vegaEmbed){vegaEmbed('#$divid',")
     Vega.our_json_print(io, v)
     print(io, ",{mode:'vega'}).catch(console.warn);})</script>")
 end
